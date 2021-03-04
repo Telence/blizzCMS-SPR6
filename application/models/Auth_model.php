6df9a88d48ceea6d853a2edfd64a8195d9b41d44
Darthar <duelista16@gmail.com>
2021-03-04 15:27:37 +0100

    Deprecated functions $this->Battlenet and  $this->Account ... New fuction added $this->game_hash

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -176,6 +176,7 @@ class Auth_model extends CI_Model {
         redirect(base_url(),'refresh');
     }
 
+    /*
     public function Battlenet($email, $password)
     {
         return strtoupper(bin2hex(strrev(hex2bin(strtoupper(hash("sha256",strtoupper(hash("sha256", strtoupper($email)).":".strtoupper($password))))))));
@@ -192,6 +193,42 @@ class Auth_model extends CI_Model {
         $sha_pass_hash = sha1(strtoupper($username).':'.strtoupper($password));
 
         return strtoupper($sha_pass_hash);
+    }
+    */
+
+    public function game_hash($username, $password, $type = null, $salt = null)
+    {
+        switch ($type)
+		{
+			case 'bnet':
+				return strtoupper(bin2hex(strrev(hex2bin(strtoupper(hash('sha256', strtoupper(hash('sha256', strtoupper($username)) . ':' . strtoupper($password))))))));
+				break;
+			case 'hex':
+				$client = new UserClient($username, $salt);
+				return strtoupper($client->generateVerifier($password));
+			case 'srp6':
+				// Constants
+				$g = gmp_init(7);
+				$N = gmp_init('894B645E89E1535BBDAD5B8B290650530801B18EBFBF5E8FAB3C82872A3E9BB7', 16);
+				// Calculate first hash
+				$h1 = sha1(strtoupper($username.':'.$password), TRUE);
+				// Calculate second hash
+				$h2 = sha1($salt.$h1, TRUE);
+				// Convert to integer (little-endian)
+				$h2 = gmp_import($h2, 1, GMP_LSW_FIRST);
+				// g^h2 mod N
+				$verifier = gmp_powm($g, $h2, $N);
+				// Convert back to a byte array (little-endian)
+				$verifier = gmp_export($verifier, 1, GMP_LSW_FIRST);
+				// Pad to 32 bytes, remember that zeros go on the end in little-endian!
+				$verifier = str_pad($verifier, 32, chr(0), STR_PAD_RIGHT);
+				return $verifier;
+				break;
+			default:
+				return strtoupper(sha1(strtoupper($username) . ':' . strtoupper($password)));
+				break;
+		}
+
     }
 
     public function checkAccountExist()
